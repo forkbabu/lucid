f5101c68f7b56ff8276a617eba9c2b91ea497279
Forkbabu <sayantandas30011998@gmail.com>
2020-09-02 12:55:15 +0530

    Update style.py

--- lucid/optvis/style.py
+++ lucid/optvis/style.py
@@ -8,18 +8,18 @@ def gram_style(a):
     chn = int(a.shape[-1])
     a = tf.reshape(a, [-1, chn])
     n = tf.shape(a)[0]
-    gram = tf.matmul(a, a, transpose_a=True)
+    gram = tf.linalg.matmul(a, a, transpose_a=True)
     return gram / tf.cast(n, tf.float32)
 
 
 def mean_l1_loss(g1, g2):
   with tf.name_scope('mean_l1_loss'):
-    return tf.reduce_mean(tf.abs(g1-g2))
+    return tf.math.reduce_mean(tf.abs(g1-g2))
 
 
 def mean_l2_loss(g1, g2):
   with tf.name_scope('mean_l2_loss'):
-    return tf.sqrt(tf.reduce_mean(tf.square(g1 - g2)))
+    return tf.math.sqrt(tf.math.reduce_mean(tf.math.square(g1 - g2)))
 
 
 class StyleLoss(object):
@@ -53,7 +53,7 @@ class StyleLoss(object):
     
     if ema_decay is not None:
       # creating moving average versions of input Gram matrices
-      self.ema = tf.train.ExponentialMovingAverage(decay=ema_decay)
+      self.ema = tf.compat.v1.train.ExponentialMovingAverage(decay=ema_decay)
       update_ema_op = self.ema.apply(self.input_grams)
       # averages get updated before evaluation of effective_grams
       with tf.control_dependencies([update_ema_op]):
@@ -69,7 +69,7 @@ class StyleLoss(object):
                         for g in self.input_grams]
     self.style_losses = [loss_func(g, gv)
                          for g, gv in zip(self.effective_grams, self.target_vars)]
-    self.style_loss = tf.add_n(self.style_losses)
+    self.style_loss = tf.math.add_n(self.style_losses)
 
   def set_style(self, input_feeds):
     """Set target style variables.
@@ -77,14 +77,14 @@ class StyleLoss(object):
     Expected usage: 
       style_loss = StyleLoss(style_layers)
       ...
-      init_op = tf.global_variables_initializer()
+      init_op = tf.compat.v1.global_variables_initializer()
       init_op.run()
       
       feeds = {... session.run() 'feeds' argument that will make 'style_layers'
                tensors evaluate to activation values of style image...}
       style_loss.set_style(feeds)  # this must be called after 'init_op.run()'
     """
-    sess = tf.get_default_session()
+    sess = tf.compat.v1.get_default_session()
     computed = sess.run(self.input_grams, input_feeds)
     for v, g in zip(self.target_vars, computed):
       v.load(g)
